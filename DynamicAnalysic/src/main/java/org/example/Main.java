9369750a2faab9992f4b5c327757621ee460ff74
Heberto Rodriguez <hebertorodriguez@mail.weber.edu>
2024-08-14 15:14:52 -0600

    Add files via upload

---
+++ DynamicAnalysic/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Path to the jacoco.exec file from the Validator program
+        Process process = getProcess();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        StringBuilder output = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            output.append(line).append("\n");
+        }
+
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(output);
+    }
+
+    private static Process getProcess() throws IOException {
+        File workingDirectory = new File("C:/Users/hrodriguez/Desktop/Heberto/CS6720/Assignments/Assignment1/Commons-validator");
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "C:/Users/hrodriguez/Desktop/Heberto/CS6720/apache-maven-3.9.8-bin/apache-maven-3.9.8/bin/mvn.cmd",
+                "-Dtest=UrlValidatorTest", "test" // First run with test then run with jacoco:report --> // test //jacoco:report
+        );
+        processBuilder.redirectErrorStream(true);
+        processBuilder.directory(workingDirectory);
+
+        return processBuilder.start();
+    }
+}
